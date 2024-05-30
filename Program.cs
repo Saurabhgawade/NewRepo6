77d0967756b68698dfe0d412e0de9c530846a3b0
Saurabhgawade <gawadesaurabh26@gmail.com>
2024-05-30 17:16:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace abstarct
+{
+    internal class Program
+    {
+
+      
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Sleep();
+            dog.Run();
+        }
+    }
+    abstract class Animal
+    {
+        public abstract void Run();
+        public void Sleep()
+        {
+            Console.WriteLine("Sleeping");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void Run()
+        {
+            Console.WriteLine("run");
+        }
+    }
+}
